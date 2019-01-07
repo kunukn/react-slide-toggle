1372fd9230c3cb51aefd8c235904f58cca77c12c
Kunuk Nykjær <kunukn@gmail.com>
2019-01-07 22:49:37 +0100

    upd webpack setup

---
+++ src/test/jestSetup.js
@@ -0,0 +1,14 @@
+import Enzyme, { shallow, render, mount, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+console.log('setupEnzyme');
+
+configure({ adapter: new Adapter() });
+
+// Make Enzyme functions available in all test files without importing
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
+global.requestAnimationFrame = function(callback) {
+  setTimeout(callback, 0);
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,91 +1,175 @@
-const path = require("path");
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+/*
+ * webpack 4+
+ * */
 
-const entry = "./src/demo/index.js";
-const name = "app";
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const webpack = require('webpack');
 
-const IsWebpackDevServer = /webpack-dev-server/.test(
-  process.env.npm_lifecycle_script
-);
+// const IsWebpackDevServer = /webpack-dev-server/.test(process.env.npm_lifecycle_script);
 
-module.exports = (env = {}, argv = { mode: "development" }) => {
-  console.log("***", argv.mode, "***");
+module.exports = (env = {}, argv = {}) => {
+  const isProd = argv.mode === 'production';
 
-  const plugins = [
-    new HtmlWebpackPlugin({
-      template: "./src/demo/index.html",
-      filename: "index.html",
-      inject: "body"
-    }),
-  ];
+  console.log('***', isProd ? 'prod' : 'dev', '***');
 
-  return {
-    entry: {
-      app: [entry]
+  let prodEntry = './src/production-entry';
+  let devEntry = './src/development-entry';
+
+  let config = {
+    devtool: isProd ? 'source-map' : 'cheap-module-source-map',
+    mode: isProd ? 'production' : 'development',
+    optimization: {
+      minimizer: [
+        isProd &&
+          new UglifyJsPlugin({
+            uglifyOptions: {
+              compress: {
+                drop_console: true,
+              },
+              output: {
+                comments: false,
+              },
+            },
+            cache: true,
+            parallel: true,
+            sourceMap: true,
+            extractComments: true,
+          }),
+        isProd &&
+          new OptimizeCSSAssetsPlugin({
+            cssProcessorOptions: {
+              sourcemap: true,
+            },
+          }),
+      ].filter(Boolean),
     },
-    devServer: {
-      open: true,
-      contentBase: "./",
-      noInfo: true,
-      port: 3399,
-      compress: false,
-      inline: true
-      // hot: true,
+    entry: {
+      ReactSlideToggle: isProd ? [prodEntry] : [devEntry],
     },
     output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "[name].js",
-      chunkFilename: "[id].chunk.js",
-      libraryTarget: "umd",
-      library: "app"
+      path: path.resolve(__dirname, 'dist'),
+      chunkFilename: '[name].js',
+      filename: '[name].js',
+      library: 'ReactSlideToggle',
+      libraryTarget: 'umd',
+      publicPath: '/',
+    },
+    devServer: {
+      //https: true,
+      port: 3399,
+      contentBase: path.join(__dirname, ''),
+      publicPath: '/',
+      open: true,
+      hot: true,
+      disableHostCheck: true,
+      watchContentBase: true,
+      historyApiFallback: true,
     },
     module: {
       rules: [
         {
-          test: /\.(js|jsx)$/,
+          test: /\.html$/,
           exclude: /node_modules/,
           use: [
             {
-              loader: "babel-loader",
-            }
-          ]
+              loader: 'html-loader',
+              options: {
+                minimize: isProd,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          include: path.join(__dirname, 'src'),
+          use: {
+            loader: 'babel-loader',
+          },
         },
         {
           test: /\.scss$/,
-          exclude: [/node_modules/],
+          exclude: /node_modules/,
+          include: path.join(__dirname, 'src'),
           use: [
             {
-              loader: "style-loader"
+              loader: isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+              options: isProd
+                ? {
+                    publicPath: './',
+                  }
+                : {},
             },
             {
-              loader: "css-loader"
-            },
-            {
-              loader: "postcss-loader",
+              loader: 'css-loader',
               options: {
-                sourceMap: true
-              }
+                sourceMap: true,
+              },
             },
             {
-              loader: "sass-loader",
+              loader: 'postcss-loader',
               options: {
-                sourceMap: true
-              }
-            }
-          ]
-        }
-      ]
+                sourceMap: true,
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
+        },
+      ].filter(Boolean),
     },
-    plugins,
+    plugins: [
+      isProd && new CleanWebpackPlugin('dist', {}),
+      new MiniCssExtractPlugin({ filename: '[name].css', chunkFilename: '[name]-[id].css' }),
+      new HtmlWebpackPlugin({
+        compile: false,
+        inject: true,
+        hash: true,
+        template: 'src/demo/index.html',
+        filename: 'index.html',
+      }),
+      // This is necessary to emit hot updates (currently CSS only):
+      !isProd && new webpack.HotModuleReplacementPlugin(),
+      new WebpackMd5Hash(),
+    ].filter(Boolean),
     resolve: {
-      extensions: [".js", ".jsx", "scss"],
+      //modules: [path.resolve(__dirname), 'node_modules'],
+      extensions: ['.js', '.jsx', '.scss'],
       alias: {
-        library: path.resolve(__dirname, "src/library"),
-        "react-slide-toggle": path.resolve(__dirname, "src/library/index.js"),
-        dist: path.resolve(__dirname, "dist"),
-      }
+        root: __dirname,
+        src: path.resolve(__dirname, 'src'),
+        components: path.resolve(__dirname, 'src/components'),
+      },
     },
-    externals: {}
+    externals: {},
   };
+
+  if (isProd) {
+    config.externals['react'] = {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react',
+      umd: 'react',
+    };
+    config.externals['react-dom'] = {
+      root: 'ReactDOM',
+      commonjs2: 'react-dom',
+      commonjs: 'react-dom',
+      amd: 'react-dom',
+      umd: 'react-dom',
+    };
+  }
+
+  return config;
 };
